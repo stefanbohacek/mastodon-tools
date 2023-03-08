21c6f0338e3a68b62b8dbe0781fc3f21be49b165
Stefan Bohacek <stefan@fourtonfish.com>
2023-03-07 21:39:02 -0500

    Added Fediverse Explorer link when viewing a tag page.

--- browser-extension/scripts/popup.js
+++ browser-extension/scripts/popup.js
@@ -1,3 +1,5 @@
+let currentURL;
+
 const ready = (fn) => {
   if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading'){
     fn();
@@ -6,32 +8,60 @@ const ready = (fn) => {
   }
 }
 
-const switchInstance = (currentTarget) => {
-  const instance = currentTarget.dataset.instance;
+const handleLinks = () => {
+  const links = document.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
+    (() => {
+        const ln = links[i];
+        const location = ln.href;
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    const currentURL = activeTab.url;
+        if (!location.includes('#')){
+          ln.onclick = () => {
+            chrome.tabs.create({active: true, url: location});
+          };
+        }
+    })();
+  }
+}
 
-    let domain = (new URL(currentURL));
-    domain = domain.hostname;
-    console.log();
+const addFediverseExplorerLink = async (currentURL) => {
+  const tagBrowserLink = document.getElementById('tag-browser-link');
+  const tag = currentURL.split('/tags/');
 
-    if (domain === instance){
-      alert(`You are already on ${instance}.`);
-    } else {
-      const switchedURL = `https://${instance}/authorize_interaction?uri=${currentURL}`;
-      chrome.tabs.update({url:switchedURL});
-    }
-    
-
+  console.log({
+    currentURL,
+    tag
   });
+
+  if (tag && tag.length > 1){
+    const a = document.createElement('a');
+    const link = document.createTextNode(`Explore the #${tag[1]} tag`);
+    a.classList.add('fw-bold');
+    a.appendChild(link); 
+    a.href = `https://fediverse-explorer.stefanbohacek.dev/?tag=${tag[1]}`; 
+    tagBrowserLink.appendChild(a);
+    tagBrowserLink.classList.remove('d-none');
+  }
+}
+
+const switchInstance = async (currentTarget) => {
+  const instance = currentTarget.dataset.instance;
+  let domain = (new URL(currentURL));
+  domain = domain.hostname;
+
+  if (domain === instance){
+    alert(`You are already on ${instance}.`);
+  } else {
+    const switchedURL = `https://${instance}/authorize_interaction?uri=${currentURL}`;
+    chrome.tabs.update({url:switchedURL});
+  }
+  window.close();
 }
 
 const addLiveEventListeners = (selector, event, handler) => {
   const rootElement = document.querySelector('body');
   rootElement.addEventListener(event, function (evt) {
-    const targetElement = evt.target;
+    let targetElement = evt.target;
     while (targetElement != null) {
       if (targetElement.matches(selector)) {
           handler(targetElement);
@@ -43,31 +73,37 @@ const addLiveEventListeners = (selector, event, handler) => {
 }
 
 ready(() => {
-  addLiveEventListeners(".switch-instance-btn", "click", switchInstance);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+    currentURL = activeTab.url;
 
-  document.getElementById('go-to-options').addEventListener('click', ()  => {
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-    } else {
-      window.open(chrome.runtime.getURL('options.html'));
-    }
+    addFediverseExplorerLink(currentURL);
+    addLiveEventListeners(".switch-instance-btn", "click", switchInstance);
+    handleLinks();
+  
+    document.getElementById('go-to-options').addEventListener('click', ()  => {
+      if (chrome.runtime.openOptionsPage) {
+        chrome.runtime.openOptionsPage();
+      } else {
+        window.open(chrome.runtime.getURL('options.html'));
+      }
+    });
+  
+    chrome.storage.sync.get({
+      mastodonInstances: 'mastodon.social'
+    }, (items)  => {
+      if (items.mastodonInstances){
+        let instanceListHTML = '';      
+        items.mastodonInstances.split('\n').forEach((instance) => {
+          instanceListHTML += `
+          <li class="pb-1">
+            <a href="#" class="switch-instance-btn w-100 text-left" data-instance=${instance}>${instance}</a>
+          </li>    
+          `;
+        });
+        const mastodonInstances = document.getElementById('mastodon-instances');
+        mastodonInstances.innerHTML = instanceListHTML;
+      }
+    }); 
   });
-
-  chrome.storage.sync.get({
-    mastodonInstances: 'mastodon.social'
-  }, (items)  => {
-    console.log(items);
-    if (items.mastodonInstances){
-      let instanceListHTML = '';      
-      items.mastodonInstances.split('\n').forEach((instance) => {
-        instanceListHTML += `
-        <li class="pb-1">
-          <button class="switch-instance-btn w-100 text-left" data-instance=${instance}>${instance}</button>
-        </li>    
-        `;
-      });
-      const mastodonInstances = document.getElementById('mastodon-instances');
-      mastodonInstances.innerHTML = instanceListHTML;
-    }
-  });  
 });
