74823de62f511c7cde00132dfc2dce3fcda2417d
Stefan Bohacek <stefan@fourtonfish.com>
2023-03-08 19:49:30 -0500

    Added hovercards to notifications page.

--- browser-extension/scripts/main.js
+++ browser-extension/scripts/main.js
@@ -5,8 +5,10 @@ import Hovercard from './modules/Hovercard.js';
 const hovercard = new Hovercard();
 
 ready(() => {
-    addLiveEventListeners(".status__display-name", "mouseenter", hovercard.addHovercard);
+    addLiveEventListeners(".account__display-name", "mouseenter", hovercard.addHovercard);
     addLiveEventListeners(".detailed-status__display-name", "mouseenter", hovercard.addHovercard);
+    addLiveEventListeners(".notification__display-name", "mouseenter", hovercard.addHovercard);
+    addLiveEventListeners(".status__display-name", "mouseenter", hovercard.addHovercard);
     // addLiveEventListeners(".status__display-name", "mouseleave", hovercard.removeHovercards);
     // addLiveEventListeners(".status__display-name", "mouseout", hovercard.removeHovercards);
     // addLiveEventListeners(".sbmt-account-info", "mouseleave", hovercard.removeHovercards);
