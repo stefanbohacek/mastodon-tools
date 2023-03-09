539b274356bc75f8afbb13154727e841b2e957cb
Stefan Bohacek <stefan@fourtonfish.com>
2023-03-09 09:52:41 -0500

    Updated icons. Minor cleanup.

--- browser-extension/scripts/main.js
+++ browser-extension/scripts/main.js
@@ -5,6 +5,7 @@ import Hovercard from './modules/Hovercard.js';
 const hovercard = new Hovercard();
 
 ready(() => {
+    // addLiveEventListeners(".mention.status-link", "mouseenter", hovercard.addHovercard);
     addLiveEventListeners(".account__display-name", "mouseenter", hovercard.addHovercard);
     addLiveEventListeners(".detailed-status__display-name", "mouseenter", hovercard.addHovercard);
     addLiveEventListeners(".notification__display-name", "mouseenter", hovercard.addHovercard);
